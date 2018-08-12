9169af8c89c9d4d7e87f8d2d796216feb012b6b1
sol20-meet <sol20@meet.mit.edu>
2018-08-12 10:44:50 -0400

    Stopping here

--- part 1 code.py
+++ part 1 code.py
@@ -9,4 +9,21 @@ window_size_y=1080
 
 turtle.setup(window_size_x,window_size_y)
 
-turtle.registar_shape("
+miss = []
+score=[]
+trash_pos=[]
+theacher_pos=[]
+
+hammer=turtle.clone()
+trash=turtle.clone()
+teachers=turtle.clone()
+
+
+turtle.register_shape("Calebb.gif")
+turtle.register_shape("backpack.gif")
+teachers.shape("Calebb.gif")
+
+turtle.bgpic("backpack.gif")
+
+turtle.mainloop()
+                      
