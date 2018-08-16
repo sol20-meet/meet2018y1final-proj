93766273e3588ac573958a8cdcefb3a222c56dd1
sol20-meet <sol20@meet.mit.edu>
2018-08-16 10:29:01 +0300

    Stopping here

--- part 1 code.py
+++ part 1 code.py
@@ -25,21 +25,35 @@ trash=turtle.clone()
 teachers=turtle.clone()
 screen=turtle.clone()
 writer=turtle.clone() 
-
+turtle.register_shape("heart.gif")
 turtle.register_shape("trash.gif")
 turtle.register_shape("Calebb.gif")
 turtle.register_shape("backpack.gif")
 teachers.shape("Calebb.gif")
-
+turtle.addshape("heart.gif")
 turtle.bgpic("backpack.gif")
 trash = turtle.clone()
 turtle.addshape("trash3.gif")
 trash.shape("trash3.gif")
 trash.penup()
-
+heart = turtle.clone()
+heart.shape("heart.gif")
+heart.penup()
+heart.goto(-700,400)
+heart2 = turtle.clone()
+heart2.shape("heart.gif")
+heart2.penup()
+heart2.goto(-770,400)
+heart3= turtle.clone()
+heart3.shape("heart.gif")
+heart3.penup()
+heart3.goto(-840,400)
+turtle.undo()
+heart.penup()
 writer.penup()
 teachers.penup()
 def show_trash():
+    
     global hole
     hole = random.randint(1,10)
     if hole == 1:
@@ -96,7 +110,7 @@ def show_caleb():
 global Death
 Death=0
 def click(x,y):
-    global c, score
+    global Death, score
     hammer.goto(x,y)
     x=hammer.pos()[0]
     y=hammer.pos()[1]
@@ -104,22 +118,45 @@ def click(x,y):
     if x in range (trash.pos()[0]-z,trash.pos()[0]+z) and y in range (trash.pos()[1]-z,trash.pos()[1]+z):
         show_trash()
         turtle.undo()
+        turtle.hideturtle()
+        turtle.penup()
         turtle.color('red')
         score+=1
+        turtle.goto(900,450)
         turtle.write(score, move=False, align='left', font= ('Arial', 30,'normal'))
-    if x in range (teachers.pos()[0]-z,teachers.pos()[0]+z) and y in range (teachers.pos()[1]-z,teachers.pos()[1]+z):
-        show_caleb()
-        score+=1
+    
     else:
         Death+=1
-        if Death >=3:
-           writer.ht()
-           FONT = ('Arial',60,'normal')
-           writer.color("white") 
-           writer.goto(-200,0)
-           writer.write("Game Over",font=FONT)
-           return quit()
-    
+        if x in range (teachers.pos()[0]-z,teachers.pos()[0]+z) and y in range (teachers.pos()[1]-z,teachers.pos()[1]+z):
+            show_caleb()
+            if Death == 1 :
+                heart.ht()
+            elif Death == 2 :
+                heart2.ht()
+            elif Death == 3 :
+                
+               heart3.ht()
+               writer.ht()
+               FONT = ('Arial',60,'normal')
+               writer.color("white") 
+               writer.goto(-200,0)
+               writer.write("Game Over",font=FONT)
+               return quit()
+        else:
+            if Death == 1 :
+               heart.ht()
+            elif Death == 2 :
+               heart2.ht()
+            elif Death == 3 :
+               
+               heart3.ht()
+               writer.ht()
+               FONT = ('Arial',60,'normal')
+               writer.color("white") 
+               writer.goto(-200,0)
+               writer.write("Game Over",font=FONT)
+               return quit()
+            
 turtle.onscreenclick(click)
 
 
